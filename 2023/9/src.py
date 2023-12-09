2056818497dd6d9dd3d38b2573b6826995473694
Tim Ledlie <timledlie@gmail.com>
2023-12-09 11:37:25 -0800

    2023 day 9 part 2

--- 2023/9/src.py
+++ 2023/9/src.py
@@ -1,17 +1,17 @@
-def predict_next_value(history):
+def predict_previous_value(history):
     if len(set(history)) == 1:
         return history[0]
 
     history_difference = []
     for i in range(len(history) - 1):
         history_difference.append(history[i + 1] - history[i])
-    return history[-1] + predict_next_value(history_difference)
+    return history[0] - predict_previous_value(history_difference)
 
 
-sum_next_values = 0
+sum_previous_values = 0
 with open("input.txt") as file:
     for line in file.readlines():
         history = [int(c) for c in line.strip().split()]
-        sum_next_values += predict_next_value(history)
+        sum_previous_values += predict_previous_value(history)
 
-print(sum_next_values)
+print(sum_previous_values)
