27630a1329f02b25b7049567902a9211e670384e
Tim Ledlie <timledlie@gmail.com>
2023-12-13 10:26:13 -0800

    2023 day 13 part 2

--- 2023/13/src.py
+++ 2023/13/src.py
@@ -1,27 +1,34 @@
-def rotate_clockwise_90(pattern):
-    rotated_rows = [[] for _ in range(len(pattern[0]))]
-    for row in range(len(pattern)):
-        for col in range(len(pattern[0])):
-            rotated_rows[col].append(pattern[row][col])
-    return rotated_rows
+def convert_to_sets(pattern_as_tuples):
+    pattern_as_sets = []
+    for pattern_tuple in pattern_as_tuples:
+        rock_indexes = set()
+        for i in range(len(pattern_tuple)):
+            if pattern_tuple[i] == "#":
+                rock_indexes.add(i)
+        pattern_as_sets.append(rock_indexes)
+    return pattern_as_sets
 
 
-def find_reflection_index(pattern):
+def find_reflection_index(pattern, n_off_by):
     for index in range(len(pattern) - 1):
-        is_reflection_point = True
+        running_off_by = 0
         for a, b in zip(range(index, -1, -1), range(index + 1, len(pattern))):
-            if pattern[a] != pattern[b]:
-                is_reflection_point = False
-        if is_reflection_point:
+            running_off_by += len(pattern[a] ^ pattern[b])
+            if running_off_by > n_off_by:
+                break
+        if running_off_by == n_off_by:
             return index + 1  # the problem statement indexes starting at 1
     return 0
 
 
-patterns = []
 total = 0
+n_off_by = 1
 with open("input.txt") as file:
     for pattern_text in file.read().split("\n\n"):
-        pattern = [list(row) for row in pattern_text.strip().split()]
-        total += 100 * find_reflection_index(pattern) + find_reflection_index(rotate_clockwise_90(pattern))
+        pattern_orig = [tuple(char) for char in pattern_text.strip().split()]
+        pattern_rotated = [*zip(*pattern_orig)]  # reflect rows into columns
+        pattern_orig = convert_to_sets(pattern_orig)
+        pattern_rotated = convert_to_sets(pattern_rotated)
+        total += 100 * find_reflection_index(pattern_orig, n_off_by) + find_reflection_index(pattern_rotated, n_off_by)
 
 print(total)
