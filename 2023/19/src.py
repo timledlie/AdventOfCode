244c8e1828e0de7daa247d3632d472230129b955
Tim Ledlie <timledlie@gmail.com>
2023-12-28 12:05:25 -0800

    2023 day 19 part 2

--- 2023/19/src.py
+++ 2023/19/src.py
@@ -2,6 +2,9 @@ class Workflow:
     def __init__(self, rules):
         self.rules = rules
 
+    def __repr__(self):
+        return ", ".join([rule.__repr__() for rule in self.rules])
+
 
 class Rule:
     def __init__(self, part_category, operation, value, part_destination):
@@ -10,42 +13,15 @@ class Rule:
         self.value = value
         self.part_destination = part_destination
 
-
-class PartRating:
-    def __init__(self, ratings_dict):
-        self.ratings_dict = ratings_dict
-
-    def get_value(self):
-        return self.ratings_dict["x"] + self.ratings_dict["m"] + self.ratings_dict["a"] + self.ratings_dict["s"]
-
-
-def is_rule_passes(part_rating, rule):
-    if rule.operation == ">":
-        return part_rating.ratings_dict[rule.part_category] > rule.value
-    return part_rating.ratings_dict[rule.part_category] < rule.value
-
-
-def is_part_accepted(part_rating, workflows):
-    state = "in"
-    while True:
-        workflow = workflows[state]
-        for rule in workflow.rules:
-            if is_rule_passes(part_rating, rule):
-                state = rule.part_destination
-                if state == "A":
-                    return True
-                if state == "R":
-                    return False
-                break
-    exit("poop")
+    def __repr__(self):
+        return self.part_category + " " + self.operation + " " + str(self.value) + ": " + self.part_destination
 
 
 workflows = {}
-part_ratings = []
 with open("input.txt") as file:
     text = file.read()
 
-workflows_text, part_ratings_text = text.strip().split("\n\n")
+workflows_text = text.strip().split("\n\n")[0]
 
 for workflow_line in workflows_text.split("\n"):
     name, rest = workflow_line.split("{")
@@ -60,17 +36,55 @@ for workflow_line in workflows_text.split("\n"):
     rules.append(Rule("x", ">", 0, default))  # the default is a rule that is always True
     workflows[name] = Workflow(rules)
 
-part_ratings = []
-for part_ratings_line in part_ratings_text.split("\n"):
-    ratings_dict = {}
-    for part_rating in part_ratings_line[1:-1].split(","):
-        part_category, value = part_rating.split("=")
-        ratings_dict[part_category] = int(value)
-    part_ratings.append(PartRating(ratings_dict))
 
-accepted_total = 0
-for part_rating in part_ratings:
-    if is_part_accepted(part_rating, workflows):
-        accepted_total += part_rating.get_value()
+def find_accepted_paths(node):
+    if node == "A":
+        return [[("x", ">", 0)]]
+    if node == "R":
+        return None
 
-print(accepted_total)
+    prerequisites = []
+    permutations = []
+    for rule in workflows[node].rules:
+        rule_pass = (rule.part_category, rule.operation, rule.value)
+        recursion_permutations = find_accepted_paths(rule.part_destination)
+        if recursion_permutations is not None:
+            permutations += [prerequisites + [rule_pass] + p for p in recursion_permutations]
+
+        if rule.operation == "<":
+            fail_operation = ">="
+        else:
+            fail_operation = "<="
+        rule_fail = (rule.part_category, fail_operation, rule.value)
+        prerequisites.append(rule_fail)
+
+    return permutations
+
+
+combinations = 0
+max_default = 4000
+paths = find_accepted_paths("in")
+for path in paths:
+    mins = {"x": 1, "m": 1, "a": 1, "s": 1}
+    maxes = {"x": max_default, "m": max_default, "a": max_default, "s": max_default}
+    for category, operation, value in path:
+        if operation == "<":
+            if maxes[category] >= value:
+                maxes[category] = value - 1
+        elif operation == "<=":
+            if maxes[category] > value:
+                maxes[category] = value
+        elif operation == ">":
+            if mins[category] <= value:
+                mins[category] = value + 1
+        elif operation == ">=":
+            if mins[category] < value:
+                mins[category] = value
+    product = 1
+    for category in ("x", "m", "a", "s"):
+        n_permutations = maxes[category] - mins[category] + 1
+        if n_permutations < 0:
+            n_permutations = 0
+        product *= n_permutations
+    combinations += product
+print(combinations)
