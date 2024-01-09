98d4060eccebf06a2557c031b737bc7ae5e5d525
Tim Ledlie <timledlie@gmail.com>
2024-01-08 21:50:12 -0800

    2023 day 22 part 2

--- 2023/22/src.py
+++ 2023/22/src.py
@@ -4,7 +4,7 @@ import copy
 
 def get_brick_footprint(brick):
     footprint = set()
-    (x1, y1, z1), (x2, y2, z2) = brick
+    (x1, y1, z1), (x2, y2, z2), id = brick
     if x1 != x2:
         for x in range(x1, x2 + 1):
             footprint.add((x, y1))
@@ -23,16 +23,16 @@ def drop_brick(highest_blocks, brick):
     for x, y in footprint:
         highest_blocks[(x, y)] = z_landing
 
-    (x1, y1, z1), (x2, y2, z2) = brick
+    (x1, y1, z1), (x2, y2, z2), id = brick
     if z2 > z1:
         highest_blocks[(x, y)] += (z2 - z1)
 
-    return (x1, y1, z_landing), (x2, y2, z2 - z1 + z_landing)
+    return (x1, y1, z_landing), (x2, y2, z2 - z1 + z_landing), id
 
 
 def drop_bricks(bricks):
     # sort by z
-    bricks.sort(key=lambda x: min(x[0][2], x[1][2]))
+    bricks.sort(key=lambda b: min(b[0][2], b[1][2]))
     highest_blocks = defaultdict(int)
     settled_bricks = []
     for brick in bricks:
@@ -43,20 +43,21 @@ def drop_bricks(bricks):
 bricks = []
 x_max, y_max = 0, 0
 with open("input.txt") as file:
+    id = 1
     for line in file.readlines():
         a, b = line.strip().split("~")
         x1, y1, z1 = tuple(map(int, a.split(",")))
         x2, y2, z2 = tuple(map(int, b.split(",")))
         x_max = max(x_max, x1, x2)
         y_max = max(y_max, y1, y2)
-        bricks.append(((x1, y1, z1), (x2, y2, z2)))
+        bricks.append(((x1, y1, z1), (x2, y2, z2), id))
+        id += 1
 
-n_disintegratable_bricks = 0
+n_falling_bricks = 0
 settled_bricks = drop_bricks(bricks)
 for brick in settled_bricks:
     bricks = copy.copy(settled_bricks)
     bricks.remove(brick)
-    if bricks == drop_bricks(bricks):
-        n_disintegratable_bricks += 1
+    n_falling_bricks += len(set(drop_bricks(bricks)) - set(bricks))
 
-print(n_disintegratable_bricks)
+print(n_falling_bricks)
