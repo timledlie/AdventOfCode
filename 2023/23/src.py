9be1199c2b4a66ce167d32b1ede064beae1a92b0
Tim Ledlie <timledlie@gmail.com>
2024-01-10 11:59:12 -0800

    2023 day 23 part 2

--- 2023/23/src.py
+++ 2023/23/src.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 path_locations = {}
 with open("input.txt") as file:
     lines = file.readlines()
@@ -13,29 +15,51 @@ with open("input.txt") as file:
 def get_valid_neighbors(location, locations_traversed):
     x, y = location
     valid_neighbors = []
-    for dx, dy, uphill in ((1, 0, "<"), (-1, 0, ">"), (0, 1, "^"), (0, -1, "v")):
+    for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
         neighbor = x + dx, y + dy
-        if (neighbor in path_locations) and (path_locations[neighbor] != uphill) and (neighbor not in locations_traversed):
+        if (neighbor in path_locations) and (neighbor not in locations_traversed):
             valid_neighbors.append(neighbor)
 
     return valid_neighbors
 
 
-def longest_path(start, end, locations_traversed):
-    n_steps = 0
-    cur = start
-    while True:
-        valid_neighbors = get_valid_neighbors(cur, locations_traversed)
-        if len(valid_neighbors) == 0:
-            return 0
-        elif len(valid_neighbors) == 1:
-            n_steps += 1
-            cur = valid_neighbors[0]
-            locations_traversed.append(cur)
-            if cur == end:
-                return n_steps
-        else:
-            return n_steps + 1 + max([longest_path(neighbor, end, locations_traversed + [neighbor]) for neighbor in valid_neighbors])
+def longest_path(cur, end, locations_traversed):
+    if cur == end:
+        return 0
+    next_max_distances = []
+    for next_node, distance in edges[cur].items():
+        if next_node not in locations_traversed:
+            rest_max_distance = longest_path(next_node, end, locations_traversed + [next_node])
+            if rest_max_distance is not None:
+                next_max_distances.append(distance + rest_max_distance)
+    if len(next_max_distances) > 0:
+        return max(next_max_distances)
+    else:
+        return None
 
+
+nodes = {start, end}
+for location, path_type in path_locations.items():
+    two_right = (location[0] + 2, location[1])
+    if (path_type == ">") and (two_right in path_locations) and (path_locations[two_right] == ">"):
+        nodes.add((location[0] + 1, location[1]))
+
+    two_down = (location[0], location[1] + 2)
+    if (path_type == "v") and (two_down in path_locations) and (path_locations[two_down] == "v"):
+        nodes.add((location[0], location[1] + 1))
+
+edges = defaultdict(dict)  # {nodeA: {nodeB: weight,...}, ...}
+for node in nodes:
+    for cur in get_valid_neighbors(node, []):
+        locations_traversed = [node, cur]
+        n_steps = 1
+        while True:
+            cur = get_valid_neighbors(cur, locations_traversed)[0]
+            n_steps += 1
+            locations_traversed.append(cur)
+            if cur in nodes:
+                edges[node][cur] = n_steps
+                edges[cur][node] = n_steps
+                break
 
 print(longest_path(start, end, [start]))
