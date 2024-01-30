bd10d76781dc1255e180789660cb9fa8b8f2b386
Tim Ledlie <timledlie@gmail.com>
2024-01-29 21:10:26 -0800

    2019 day 14 part 1

--- 2019/14/src.py
+++ 2019/14/src.py
@@ -1,18 +1,12 @@
-# depths: [int(str(d).strip()) for d in f.readlines()]
-# import numpy as np
-# from collections import defaultdict
+from collections import defaultdict
 from collections import namedtuple
-# PasswordCheck = namedtuple('PasswordCheck', ['span', 'letter', 'password'])
-# from collections import deque
-# import re
-# import itertools
-# import copy
 
 Element = namedtuple('Element', ['name', 'quantity'])
 Reaction = namedtuple('Reaction', ['output', 'inputs'])
+remainders = defaultdict(int)
 
 reactions = {}
-with open("input_sample.txt") as file:
+with open("input.txt") as file:
     for line in file.readlines():
         inputs, output = line.split(' => ')
         input_elements = []
@@ -22,10 +16,8 @@ with open("input_sample.txt") as file:
         output_quantity, output_name = output.split()
         reactions[output_name] = Reaction(Element(output_name, int(output_quantity)), input_elements)
 
-print(reactions)
-
 elements_needed = reactions['FUEL'].inputs
-print(elements_needed)
+
 
 def is_more_resolution_needed(elements):
     for element in elements:
@@ -34,37 +26,31 @@ def is_more_resolution_needed(elements):
     return False
 
 
-def merge_ore(elements):
-    ret = []
-    ore_quantity = 0
-    for element in elements:
-        if element.name == 'ORE':
-            ore_quantity += element.quantity
-        else:
-            ret.append(element)
-    if ore_quantity:
-        ret.append(Element('ORE', ore_quantity))
-    return ret
+def merge_elements(elements_dict):
+    return [Element(name, quantity) for name, quantity in elements_dict.items()]
 
 
 while is_more_resolution_needed(elements_needed):
-    new_elements_needed = []
+    new_elements_needed = defaultdict(int)
     for element in elements_needed:
         if element.name == 'ORE':
-            new_elements_needed.append(element)
+            new_elements_needed[element.name] += element.quantity
         else:
             output_needed_name = element.name
             output_needed_quantity = element.quantity
-            relevant_reaction = reactions[element.name]
-
-            output_quantity = relevant_reaction.output.quantity
-            if output_needed_quantity % output_quantity == 0:
-                multiplier = output_needed_quantity // output_quantity
+            if output_needed_quantity <= remainders[output_needed_name]:
+                remainders[output_needed_name] -= output_needed_quantity
             else:
-                multiplier = output_needed_quantity // output_quantity + 1
+                relevant_reaction = reactions[output_needed_name]
+                output_quantity = relevant_reaction.output.quantity
+                if (output_needed_quantity - remainders[output_needed_name]) % output_quantity == 0:
+                    multiplier = (output_needed_quantity - remainders[output_needed_name]) // output_quantity
+                else:
+                    multiplier = (output_needed_quantity - remainders[output_needed_name]) // output_quantity + 1
+                remainders[output_needed_name] += (output_quantity * multiplier) - output_needed_quantity
 
-            for input in relevant_reaction.inputs:
-                new_elements_needed.append(Element(input.name, multiplier * input.quantity))
-    elements_needed = merge_ore(new_elements_needed)
+                for input in relevant_reaction.inputs:
+                    new_elements_needed[input.name] += multiplier * input.quantity
+    elements_needed = merge_elements(new_elements_needed)
 
-print(elements_needed)
+print(elements_needed[0].quantity)
