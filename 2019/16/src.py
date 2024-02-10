9282ab43f49407d98aeef9d197565ec58cb7aed8
Tim Ledlie <timledlie@gmail.com>
2024-02-10 16:30:01 -0500

    2019 day 16 part 1

--- 2019/16/src.py
+++ 2019/16/src.py
@@ -1,36 +1,19 @@
-# depths: [int(str(d).strip()) for d in f.readlines()]
-# import numpy as np
-# from collections import defaultdict
-# from collections import namedtuple
-# PasswordCheck = namedtuple('PasswordCheck', ['span', 'letter', 'password'])
-# from collections import deque
-# import re
-# import itertools
-# import copy
+from itertools import repeat
 
 with open("input.txt") as file:
     signal = [int(el) for el in list(file.readline().strip())]
 
 
 def make_pattern(n):
-    if n == 1:
-        return (1, 1), (0, 1), (-1, 1), (0, 1)
-    return (0, n - 1), (1, n), (0, n), (-1, n), (0, 1)
+    return tuple(repeat(0, n - 1)) + tuple(repeat(1, n)) + tuple(repeat(0, n)) + tuple(repeat(-1, n)) + (0,)
 
-
-print(make_pattern(1))
-print(make_pattern(2))
-print(make_pattern(3))
-print(make_pattern(4))
-print(make_pattern(5))
-exit()
 
 length_multiplier = 1
-for phase in range(1, 101):
+n_phases = 100
+for phase in range(1, n_phases + 1):
     signal_next = []
     for n in range(1, len(signal) + 1):
         pattern = make_pattern(n)
-        print(pattern)
         sum = n_chars = 0
         signal_index = pattern_index = 0
 
@@ -47,4 +30,4 @@ for phase in range(1, 101):
         signal_next.append(abs(sum) % 10)
 
     signal = signal_next
-    print("After", phase, "phase:", ''.join(str(i) for i in signal[:8]))
+print("After", phase, "phase:", ''.join(str(i) for i in signal[:8]))
