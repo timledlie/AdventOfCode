3ed0a7054410469c07f6b0445187349369a812c5
Tim Ledlie <timledlie@gmail.com>
2024-12-02 09:41:44 -0800

    2024 day 2 part 1

---
+++ 2024/2/src.py
@@ -0,0 +1,29 @@
+reports = []
+with open("input.txt") as file:
+    for line in file.readlines():
+        line = line.strip()
+        reports.append([int(level) for level in line.split()])
+
+
+def is_report_safe(report):
+    is_increasing = report[0] < report[1]
+    for i in range(len(report) - 1):
+        a, b = report[i], report[i + 1]
+        if abs(a - b) > 3:
+            return False
+        if a == b:
+            return False
+        if is_increasing and (a > b):
+            return False
+        if not is_increasing and (a < b):
+            return False
+
+    return True
+
+
+count_safe = 0
+for report in reports:
+    if is_report_safe(report):
+        count_safe += 1
+
+print(count_safe)
