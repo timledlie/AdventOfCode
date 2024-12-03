4b8ff374a10b3bea2e2a1074b5cd8694c47edd0b
Tim Ledlie <timledlie@gmail.com>
2024-12-03 12:03:49 -0800

    2024 day 3 part 2

--- 2024/3/src.py
+++ 2024/3/src.py
@@ -3,12 +3,13 @@ import re
 with open("input.txt") as file:
     memory = file.read()
 
-matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory)
-print(matches)
-
 multiplications_sum = 0
-for match in matches:
-    a, b = match[4:-1].split(',')
-    multiplications_sum += int(a) * int(b)
+on_segments = memory.split("do()")
+for on_segment in on_segments:
+    on_subsegment = on_segment.split("don't()")[0]
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", on_subsegment)
+    for match in matches:
+        a, b = match[4:-1].split(',')
+        multiplications_sum += int(a) * int(b)
 
 print(multiplications_sum)
