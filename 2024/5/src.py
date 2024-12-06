0f21fa7f0912085c30146a8cf255a1711e5cfb69
Tim Ledlie <timledlie@gmail.com>
2024-12-05 22:11:55 -0800

    2024 day 5 part 2

--- 2024/5/src.py
+++ 2024/5/src.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 with open("input.txt") as file:
     lines = file.read()
 
@@ -12,14 +14,18 @@ pages_to_produce = []
 for page_text in pages_text.split():
     pages_to_produce.append([int(page) for page in page_text.split(',')])
 
+
+# the ordering rules define a custom sort order for the pages
+def custom_sort(a, b):
+    return -1 if (a, b) in ordering_rules else 1
+
+
 middle_sums = 0
 for pages in pages_to_produce:
-    is_valid_ordering = True
     for i in range(len(pages) - 1):
         if (pages[i], pages[i + 1]) not in ordering_rules:
-            is_valid_ordering = False
+            pages_sorted = sorted(pages, key=cmp_to_key(custom_sort))
+            middle_sums += pages_sorted[len(pages_sorted) // 2]
             break
-    if is_valid_ordering:
-        middle_sums += pages[len(pages) // 2]
 
 print(middle_sums)
