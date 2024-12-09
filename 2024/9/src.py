14376ee539be7fea698a683b98a36a9e5c217ed0
Tim Ledlie <timledlie@gmail.com>
2024-12-09 08:51:46 -0800

    2024 day 9 part 2

--- 2024/9/src.py
+++ 2024/9/src.py
@@ -2,42 +2,45 @@ with open("input.txt") as file:
     disk_map = [int(char) for char in file.read()]
 
 blocks = []
-
+file_info = {}
+file_id = None
 for i in range(len(disk_map)):
     if i % 2 == 0:
-        blocks += [i // 2] * disk_map[i]
+        file_id = i // 2
+        block_start = len(blocks)
+        blocks += [file_id] * disk_map[i]
+        file_info[file_id] = {"start": block_start, "length": disk_map[i]}
     else:
         blocks += ["."] * disk_map[i]
+max_file_id = file_id
 
-left_index = None
-for i in range(len(blocks)):
-    if blocks[i] == ".":
-        left_index = i
-        break
 
-right_index = None
-for i in range(len(blocks) - 1, -1, -1):
-    if blocks[i] != ".":
-        right_index = i
-        break
+def find_free_space(blocks, length, rightmost_index):
+    index = 0
+    start, end = None, None
+    while index < rightmost_index:
+        if blocks[index] == ".":
+            if start is None:
+                start = index
+            end = index + 1
+            if end - start == length:
+                return start
+        else:
+            start, end = None, None
+        index += 1
+    return None
 
-while left_index < right_index:
-    blocks[left_index] = blocks[right_index]
-    blocks[right_index] = "."
 
-    left_index += 1
-    right_index -= 1
-
-    while (blocks[left_index] != ".") and (left_index <= right_index):
-        left_index += 1
-
-    while (blocks[right_index] == ".") and (left_index <= right_index):
-        right_index -= 1
+for file_id in range(max_file_id, 0, -1):
+    index = find_free_space(blocks, file_info[file_id]["length"], file_info[file_id]["start"])
+    if index is not None:
+        for offset in range(file_info[file_id]["length"]):
+            blocks[index + offset] = file_id
+            blocks[file_info[file_id]["start"] + offset] = "."
 
 checksum = 0
 for i in range(len(blocks)):
-    if blocks[i] == ".":
-        break
-    checksum += i * blocks[i]
+    if blocks[i] != ".":
+        checksum += i * blocks[i]
 
 print(checksum)
