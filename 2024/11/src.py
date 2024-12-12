ab948e2f815a3696b9ba9e0afa6b2029490e7cc8
Tim Ledlie <timledlie@gmail.com>
2024-12-12 08:33:33 -0800

    2024 day 11 part 2

--- 2024/11/src.py
+++ 2024/11/src.py
@@ -1,16 +1,25 @@
+from collections import defaultdict
+
+stone_counts = defaultdict(int)
 with open("input.txt") as file:
-    stones = [int(stone) for stone in file.read().strip().split()]
+    for stone in file.read().strip().split():
+        stone_counts[int(stone)] = 1
 
-blinks = 25
+blinks = 75
 for blink in range(blinks):
-    next_stones = []
-    for stone in stones:
+    next_stone_counts = defaultdict(int)
+    for stone in stone_counts.keys():
         if stone == 0:
-            next_stones.append(1)
+            next_stone_counts[1] += stone_counts[stone]
         elif len(str(stone)) % 2 == 0:
-            next_stones.extend([int(str(stone)[:len(str(stone)) // 2]), int(str(stone)[len(str(stone)) // 2:])])
+            next_stone_counts[int(str(stone)[:len(str(stone)) // 2])] += stone_counts[stone]
+            next_stone_counts[int(str(stone)[len(str(stone)) // 2:])] += stone_counts[stone]
         else:
-            next_stones.append(2024 * stone)
-    stones = next_stones
+            next_stone_counts[2024 * stone] += stone_counts[stone]
+    stone_counts = next_stone_counts
 
-print(len(stones))
+total_stones = 0
+for stone in stone_counts.keys():
+    total_stones += stone_counts[stone]
+
+print(total_stones)
