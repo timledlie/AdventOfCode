dedce55413638b1b8a9b9966c2dc7e55c74d66c7
Tim Ledlie <timledlie@gmail.com>
2025-01-20 13:37:17 -0800

    2024 day 21 part 2

--- 2024/21/src.py
+++ 2024/21/src.py
@@ -1,4 +1,5 @@
 import itertools
+import copy
 
 with open("input.txt") as file:
     codes = [line.strip() for line in file.readlines()]
@@ -53,11 +54,6 @@ def get_all_paths(keypad):
     return keypad_paths
 
 
-def filter_shortest(paths):
-    min_length = min(len(path) for path in paths)
-    return list(filter(lambda path: len(path) == min_length, paths))
-
-
 def get_keypad_min_paths(keypad, code):
     paths = [path + 'A' for path in keypad[('A', code[0])]]
     for i in range(len(code) - 1):
@@ -67,26 +63,109 @@ def get_keypad_min_paths(keypad, code):
             for path_step in paths_step:
                 paths_next.append(path + path_step + 'A')
         paths = paths_next
-    return filter_shortest(paths)
+    return paths
+
+
+counts_map = {
+    "": 0,
+    "^": 0,
+    ">": 0,
+    "v": 0,
+    "<": 0,
+    '>^': 0,
+    '>v': 0,
+    'v>': 0,
+    'v<': 0,
+    '^>': 0,
+    '<v': 0,
+    '<^': 0,
+    '^<': 0,
+    ">>^": 0,
+    "v<<": 0,
+    ">^>": 0,
+    "<v<": 0
+}
+
+
+def convert_path_to_map(path):
+    counts = copy.copy(counts_map)
+    for pattern in path.split('A')[:-1]:
+        counts[pattern] += 1
+    return counts
+
+
+# For tie-breaks (eg. <v vs v<), I ran simulations and selected for the shortest resulting patterns
+def get_next_pattern(pattern):
+    if pattern == '':
+        return '',
+    if pattern == '^':
+        return '<', '>'
+    if pattern == '>':
+        return 'v', '^'
+    if pattern == 'v':
+        return '<v', '^>'
+    if pattern == '<':
+        return 'v<<', '>>^'
+    if pattern == '^>':
+        return '<', 'v>', '^'
+    if pattern == '^<':
+        return '<', 'v<', '>>^'
+    if pattern == '>^':
+        return 'v', '<^', '>'
+    if pattern == '>v':
+        return 'v', '<', '^>'
+    if pattern == 'v>':
+        return '<v', '>', '^'
+    if pattern == 'v<':
+        return '<v', '<', '>>^'
+    if pattern == '<^':
+        return 'v<<', '>^', '>'  # only case where >^ used instead of ^> (since ^> is invalid here)
+    if pattern == '<v':
+        return 'v<<', '>', '^>'
+    if pattern == '>>^':
+        return 'v', '', '<^', '>'
+    if pattern == 'v<<':
+        return '<v', '<', '', '>>^'
+    if pattern == '>^>':
+        return 'v', '<^', 'v>', '^'
+    if pattern == '<v<':
+        return 'v<<', '>', '<', '>>^'
+
+
+def get_path_map_length(path_map):
+    length = 0
+    for pattern, n in path_map.items():
+        length += n * (len(pattern) + 1)
+    return length
 
 
 numeric_keypad_paths = get_all_paths(numeric_keypad)
 directional_keypad_paths = get_all_paths(directional_keypad)
 
+n_rounds = 25
 sum_complexities = 0
 for code in codes:
-    paths1 = get_keypad_min_paths(numeric_keypad_paths, code)
+    paths = get_keypad_min_paths(numeric_keypad_paths, code)
 
-    paths2 = []
-    for path in paths1:
-        paths2.extend(get_keypad_min_paths(directional_keypad_paths, path))
-    paths2 = filter_shortest(paths2)
+    paths_next = []
+    for path in paths:
+        more_paths = get_keypad_min_paths(directional_keypad_paths, path)
+        paths_next.extend(more_paths)
+    paths = paths_next
 
-    paths3 = []
-    for path in paths2:
-        paths3.extend(get_keypad_min_paths(directional_keypad_paths, path))
-    paths3 = filter_shortest(paths3)
+    path_maps = [convert_path_to_map(path) for path in paths]
 
-    sum_complexities += (int(code[:-1]) * len(paths3[0]))
+    for r in range(n_rounds - 1):
+        path_maps_next = []
+        for path_map in path_maps:
+            path_map_next = copy.copy(counts_map)
+            for pattern, n in path_map.items():
+                for p in get_next_pattern(pattern):
+                    path_map_next[p] += n
+            path_maps_next.append(path_map_next)
+        path_maps = path_maps_next
+
+    min_length = min(get_path_map_length(pm) for pm in path_maps)
+    sum_complexities += int(code[:-1]) * min_length
 
 print(sum_complexities)
