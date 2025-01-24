b6ad601d6b6f2cb56a3df229e3273f7298f3ae12
Tim Ledlie <timledlie@gmail.com>
2025-01-23 20:26:21 -0800

    2024 day 23 part 2

--- 2024/23/src.py
+++ 2024/23/src.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-import itertools
 
 graph = defaultdict(set)
 with open("input.txt") as file:
@@ -8,11 +7,20 @@ with open("input.txt") as file:
         graph[a].add(b)
         graph[b].add(a)
 
-three_sets = set()
-for c1 in graph.keys():
-    if c1.startswith('t'):
-        for c2, c3 in itertools.combinations(graph[c1], 2):
-            if c2 in graph[c3]:
-                three_sets.add(frozenset((c1, c2, c3)))
+max_cliques = []
 
-print(len(three_sets))
+
+# From https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm#Without_pivoting
+def bron_kerbosch(r, p, x):
+    if len(p) == 0 and len(x) == 0:
+        max_cliques.append(r)
+        return
+    for v in p:
+        bron_kerbosch(r | {v}, p & graph[v], x & graph[v])
+        p = p - {v}
+        x = x | {v}
+
+
+bron_kerbosch(set(), set(graph.keys()), set())
+
+print(','.join(sorted(max(max_cliques, key=len))))
