48a12e97a98fffc1bb1ec864f7a4be10ba0435c6
Tim Ledlie <timledlie@gmail.com>
2025-12-01 21:33:40 -0800

    2025 day 1 part 2

--- 2025/1/src.py
+++ 2025/1/src.py
@@ -5,12 +5,22 @@ dial = 50
 n_zeros = 0
 for rotation in rotations:
     direction, amount = rotation[0], int(rotation[1:])
+    is_dial_starts_at_zero = dial == 0
+
+    # first count number of complete rotations
+    n_zeros += amount // 100
+
+    # next move the dial the incremental amount
     if direction == "L":
-        dial -= amount
+        dial -= amount % 100
     else:
-        dial += amount
-    dial %= 100
-    if dial == 0:
+        dial += amount % 100
+
+    # if we have hit zero during the incremental movement, count one more
+    if not is_dial_starts_at_zero and ((dial <= 0) or (dial >= 100)):
         n_zeros += 1
+
+    # normalize to our 0 - 99 range
+    dial %= 100
 
 print(n_zeros)
