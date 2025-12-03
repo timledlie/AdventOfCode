a2951038dab5699e0807a79048d233c06e7af9c5
Tim Ledlie <timledlie@gmail.com>
2025-12-03 12:31:31 -0800

    2025 day 3 part 2

--- 2025/3/src.py
+++ 2025/3/src.py
@@ -1,17 +1,19 @@
 with (open("input.txt") as f):
     banks = [tuple(map(int, bank.strip())) for bank in f.readlines()]
 
-sum_joltage = 0
+sum_joltages = 0
+n_digits = 12
 for bank in banks:
-    a, b = bank[0], bank[1]
-    for i in range(1, len(bank) - 1):
-        if bank[i] > a:
-            a = bank[i]
-            b = bank[i + 1]
-        elif bank[i] > b:
-            b = bank[i]
-    if bank[-1] > b:
-        b = bank[-1]
-    sum_joltage += a * 10 + b
+    max_digits = []
+    lowest_possible_index, lowest_possible_index_next = 0, 0
+    for n_left in range(n_digits, 0, -1):
+        max_digit = 0
+        for i in range(len(bank) - n_left, lowest_possible_index - 1, -1):
+            if bank[i] >= max_digit:
+                max_digit = bank[i]
+                lowest_possible_index_next = i + 1
+        max_digits.append(max_digit)
+        lowest_possible_index = lowest_possible_index_next
+    sum_joltages += int("".join(map(str, max_digits)))
 
-print(sum_joltage)
+print(sum_joltages)
