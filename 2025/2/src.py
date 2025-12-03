ec656264d049d1c37bf9743dcfe9bfc4ed7e5c94
Tim Ledlie <timledlie@gmail.com>
2025-12-02 17:19:37 -0800

    2025 day 2 part 2

--- 2025/2/src.py
+++ 2025/2/src.py
@@ -6,7 +6,9 @@ for a, b in ranges:
     for test_id in range(a, b + 1):
         test_id_str = str(test_id)
         len_test_id = len(test_id_str)
-        if (len_test_id % 2 == 0) and (test_id_str[0:len_test_id//2] == test_id_str[len_test_id//2:]):
-            sum_invalid_ids += test_id
+        for repeat_length in range(1, len_test_id // 2 + 1):
+            if test_id_str == test_id_str[:repeat_length] * (len_test_id // repeat_length):
+                sum_invalid_ids += test_id
+                break
 
 print(sum_invalid_ids)
