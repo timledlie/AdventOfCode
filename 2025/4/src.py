0bd00ccfb5e8c5256f9081995e6d362fa39c84d4
Tim Ledlie <timledlie@gmail.com>
2025-12-04 11:41:30 -0800

    2025 day 4 part 2

--- 2025/4/src.py
+++ 2025/4/src.py
@@ -1,20 +1,25 @@
 with (open("input.txt") as f):
-    grid = [line.strip() for line in f.readlines()]
+    grid = [list(line.strip()) for line in f.readlines()]
 
 n_rows, n_cols = len(grid), len(grid[0])
 
-n_accessible = 0
-for row in range(n_rows):
-    for col in range(n_cols):
-        if grid[row][col] == "@":
-            n_adjacent = 0
-            for r in (row - 1, row, row + 1):
-                for c in (col - 1, col, col + 1):
-                    if r < 0 or r >= n_rows or c < 0 or c >= n_cols or (r == row and c == col):
-                        continue
-                    if grid[r][c] == "@":
-                        n_adjacent += 1
-            if n_adjacent < 4:
-                n_accessible += 1
+n_removed = 0
+n_accessible = 1
+while n_accessible > 0:
+    n_accessible = 0
+    for row in range(n_rows):
+        for col in range(n_cols):
+            if grid[row][col] == "@":
+                n_adjacent = 0
+                for r in (row - 1, row, row + 1):
+                    for c in (col - 1, col, col + 1):
+                        if r < 0 or r >= n_rows or c < 0 or c >= n_cols or (r == row and c == col):
+                            continue
+                        if grid[r][c] == "@":
+                            n_adjacent += 1
+                if n_adjacent < 4:
+                    n_removed += 1
+                    n_accessible += 1
+                    grid[row][col] = "."
 
-print(n_accessible)
+print(n_removed)
