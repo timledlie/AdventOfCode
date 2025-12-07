a9dff431613daa51c66f41a465dc5d02782ba83f
Tim Ledlie <timledlie@gmail.com>
2025-12-07 10:05:41 -0800

    2025 day 6 part 2

--- 2025/6/src.py
+++ 2025/6/src.py
@@ -1,20 +1,31 @@
-import math
-
 with (open("input.txt") as f):
     lines = f.readlines()
 
-numbers = []
-for line in lines[:-1]:
-    numbers.append([int(n) for n in line.split()])
+numbers = [line[:-1] for line in lines[:-1]]
+operations = lines[-1]
 
-operations = lines[-1].split()
+column_widths = []
+column_start_indexes = [0]
+width = 0
+for i in range(1, len(operations)):
+    if operations[i] == ' ':
+        width += 1
+    else:
+        column_widths.append(width)
+        column_start_indexes.append(i)
+        width = 0
+column_widths.append(width + 1)
 
 grand_total = 0
-for i in range(len(operations)):
-    operands = [n[i] for n in numbers]
-    if operations[i] == "+":
-        grand_total += sum(operands)
-    else:
-        grand_total += math.prod(operands)
+operation_index = 0
+for col in range(len(column_start_indexes)):
+    start_index = column_start_indexes[col]
+    width = column_widths[col]
+    operation = operations[start_index]
+
+    column_numbers = []
+    for w in range(width):
+        column_numbers.append(''.join([numbers[row][start_index + w] for row in range(len(numbers))]))
+    grand_total += eval(operation.join(column_numbers))
 
 print(grand_total)
