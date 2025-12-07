d863f990dbea8def470498040ab7280350b0970a
Tim Ledlie <timledlie@gmail.com>
2025-12-06 17:21:18 -0800

    2025 day 5 part 2

--- 2025/5/src.py
+++ 2025/5/src.py
@@ -1,16 +1,27 @@
+from operator import itemgetter
+
 with (open("input.txt") as f):
     input_text = f.read()
 
-fresh_ranges_text, available_ingredients_text = input_text.split("\n\n")
+fresh_ranges_text, *ignore = input_text.split("\n\n")
 
 fresh_ranges = [tuple(map(int, line.split('-'))) for line in fresh_ranges_text.splitlines()]
-available_ingredients = tuple(map(int, available_ingredients_text.splitlines()))
 
-n_fresh_ingredients = 0
-for ingredient in available_ingredients:
-    for fresh_range in fresh_ranges:
-        if fresh_range[0] <= ingredient <= fresh_range[1]:
-            n_fresh_ingredients += 1
-            break
+fresh_ranges.sort(key=itemgetter(0))
+fresh_ranges_merged = []
+cur = fresh_ranges[0]
+for i_next in range(1, len(fresh_ranges)):
+    start1, end1 = cur
+    start2, end2 = fresh_ranges[i_next]
+    if start1 <= start2 <= end1:
+        cur = (start1, max(end1, end2))
+    else:
+        fresh_ranges_merged.append(cur)
+        cur = fresh_ranges[i_next]
+fresh_ranges_merged.append(cur)
 
-print(n_fresh_ingredients)
+n_ingredients = 0
+for fresh_range in fresh_ranges_merged:
+    n_ingredients += fresh_range[1] - fresh_range[0] + 1
+
+print(n_ingredients)
