6887c288128ebd735dfe10377c6be6f148aa56b8
Tim Ledlie <timledlie@gmail.com>
2025-12-12 10:30:20 -0800

    2025 day 7 part 2

--- 2025/7/src.py
+++ 2025/7/src.py
@@ -1,16 +1,16 @@
+import functools
+
 with (open("input.txt") as f):
     grid = [line.strip() for line in f.readlines()]
 
-count_splits = 0
-incoming_beams = {grid[0].index('S')}
-for row in range(2, len(grid) - 1):
-    incoming_beams_next = set()
-    for i in incoming_beams:
-        if grid[row][i] == '^':
-            count_splits += 1
-            incoming_beams_next.update((i - 1, i + 1))
-        else:
-            incoming_beams_next.add(i)
-    incoming_beams = incoming_beams_next
+@functools.cache
+def count_timelines(row, beam_col):
+    if row == len(grid):
+        return 1
 
-print(count_splits)
+    if grid[row][beam_col] == '^':
+        return count_timelines(row + 1, beam_col - 1) + count_timelines(row + 1, beam_col + 1)
+    else:
+        return count_timelines(row + 1, beam_col)
+
+print(count_timelines(2, grid[0].index('S')))
