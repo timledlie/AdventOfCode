dc3776a2d29c1fdaf1488f7f991a2ed2cacecbde
Tim Ledlie <timledlie@gmail.com>
2025-12-15 12:28:47 -0500

    2025 day 8 part 2

--- 2025/8/src.py
+++ 2025/8/src.py
@@ -12,7 +12,8 @@ distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1]
 
 map_box_to_circuit = {box: i for box, i in list(itertools.zip_longest(boxes, range(len(boxes))))}
 map_circuit_to_boxes = {circuit: [box] for box, circuit in map_box_to_circuit.items()}
-n_connections = 0
+n_cicruits = len(boxes)
+last_two_boxes = None
 for a, b in distances.keys():
     if map_box_to_circuit[a] != map_box_to_circuit[b]:
         a_circuit, b_circuit = map_box_to_circuit[a], map_box_to_circuit[b]
@@ -20,13 +21,9 @@ for a, b in distances.keys():
         for box in map_circuit_to_boxes[b_circuit]:
             map_box_to_circuit[box] = a_circuit
         map_circuit_to_boxes[b_circuit] = []
-    n_connections += 1
-    if n_connections == 1000:
+        n_cicruits -= 1
+        last_two_boxes = a, b
+    if n_cicruits == 1:
         break
 
-n_boxes_per_circuit = []
-for circuit, boxes in map_circuit_to_boxes.items():
-    if len(boxes) > 0:
-        n_boxes_per_circuit.append(len(boxes))
-
-print(math.prod(sorted(n_boxes_per_circuit, reverse=True)[:3]))
+print(last_two_boxes[0][0] * last_two_boxes[1][0])
