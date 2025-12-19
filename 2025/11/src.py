5ce50494a2aa1b6a14bef03d4d99462aae692f8e
Tim Ledlie <timledlie@gmail.com>
2025-12-19 09:36:42 -0500

    2025 day 11 part 2

--- 2025/11/src.py
+++ 2025/11/src.py
@@ -1,13 +1,21 @@
+import functools
+
 graph = {}
 with (open("input.txt") as f):
     for line in f.readlines():
         input_node, output_nodes = line.split(':')
         graph[input_node] = [node for node in output_nodes.strip().split()]
 
-def count_paths(graph, node):
+@functools.cache
+def count_paths(node, has_visited_dac, has_visited_fft):
     if node == "out":
-        return 1
+        return int(has_visited_dac and has_visited_fft)
 
-    return sum([count_paths(graph, node) for node in graph[node]])
+    if node == "dac":
+        has_visited_dac = True
+    if node == "fft":
+        has_visited_fft = True
 
-print(count_paths(graph, "you"))
+    return sum([count_paths(node, has_visited_dac, has_visited_fft) for node in graph[node]])
+
+print(count_paths("svr", False, False))
